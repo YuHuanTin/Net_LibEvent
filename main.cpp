7df63bece9b672a22f2f9ebda9307c84c9472453
23945 <2394555023@qq.com>
2022-08-12 12:31:33 +0800

    ;

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <event.h>
+//IOCP需要以下
+#include <event2/thread.h>
 
 #pragma comment(lib,"../lib/event.lib")
 #pragma comment(lib,"ws2_32.lib")
@@ -26,6 +28,7 @@ public:
     }
     evutil_socket_t CreateListenSocket(){
         auto listener = (evutil_socket_t)CreateSocket();
+            evutil_make_listen_socket_reuseable(listener);
         evutil_make_socket_nonblocking(listener);
         sockaddr_in serverAddr{
             AF_INET,
@@ -42,92 +45,114 @@ public:
     }
 
 };
-char
-rot13_char(char c)
-{
-    /* We don't want to use isalpha here; setting the locale would change
-     * which characters are considered alphabetical. */
-    if ((c >= 'a' && c <= 'm') || (c >= 'A' && c <= 'M'))
-        return c + 13;
-    else if ((c >= 'n' && c <= 'z') || (c >= 'N' && c <= 'Z'))
-        return c - 13;
-    else
-        return c;
-}
-
-void fn_read(struct bufferevent *bev, void *ctx)
-{
-    struct evbuffer *input, *output;
-    char *line;
-    size_t n;
-    int i;
-    input = bufferevent_get_input(bev);
-    output = bufferevent_get_output(bev);
-
-    while ((line = evbuffer_readln(input, &n, EVBUFFER_EOL_LF))) {
-        for (i = 0; i < n; ++i)
-            line[i] = rot13_char(line[i]);
-        evbuffer_add(output, line, n);
-        evbuffer_add(output, "\n", 1);
-        free(line);
-    }
-
-    if (evbuffer_get_length(input) >= 0x4000) {
-        /* Too long; just process what there is and go on so that the buffer
-         * doesn't grow infinitely long. */
-        char buf[1024];
-        while (evbuffer_get_length(input)) {
-            int n = evbuffer_remove(input, buf, sizeof(buf));
-            printf_s("%s",buf,n);
-            for (i = 0; i < n; ++i)
-                buf[i] = rot13_char(buf[i]);
-            evbuffer_add(output, buf, n);
+class cEvent{
+public:
+    //打印支持的网络模型
+    static void printf_supported_methods(){
+        const char **methods = event_get_supported_methods();
+        for (int i = 0; methods[i] != nullptr; ++i) {
+            printf("[+]methods:%s\n", methods[i]);
         }
-        evbuffer_add(output, "\n", 1);
     }
-}
-
-void fn_write(bufferevent *bev, void *userArgs){
-    /*
-    evbuffer *input, *output;
-    input = bufferevent_get_input(bev);
-    output = bufferevent_get_output(bev);
-*/
-    //closesocket(*(SOCKET *)&userArgs);
-}
-void fn_accept(evutil_socket_t listener, short event, void *userArgs){
-    auto mEvent_base= (event_base *)userArgs;
-
-    sockaddr client{};
-    int socketaddrLen = (int) sizeof(client);
-    auto fd = (evutil_socket_t)accept(listener, &client, &socketaddrLen);
-    if (fd == SOCKET_ERROR){
-        fn_ErrorOutput("fn_accept::accept",WSAGetLastError());
+    //打印event_base具有的特征
+    static void printf_base_features(event_base *base){
+        int getFeatures = event_base_get_features(base);
+        if (getFeatures & EV_FEATURE_ET){
+            printf("[+]EV_FEATURE_ET\n");
+        }
+        if (getFeatures & EV_FEATURE_EARLY_CLOSE){
+            printf("[+]EV_FEATURE_EARLY_CLOSE\n");
+        }
+        if (getFeatures & EV_FEATURE_FDS){//开启后不支持epoll
+            printf("[+]EV_FEATURE_FDS\n");
+        }
+        if (getFeatures & EV_FEATURE_O1){
+            printf("[+]EV_FEATURE_O1\n");
+        }
     }
-    evutil_make_socket_nonblocking(fd);
-    bufferevent *mBufferEvent;
-    mBufferEvent = bufferevent_socket_new(mEvent_base,fd,BEV_OPT_CLOSE_ON_FREE);
-    bufferevent_setcb(mBufferEvent,fn_read,fn_write, nullptr, (void *)fd);
-    bufferevent_setwatermark(mBufferEvent,EV_READ,0,0x4000);
-    bufferevent_setwatermark(mBufferEvent,EV_WRITE,0,0x4000);
-    bufferevent_enable(mBufferEvent,EV_READ | EV_WRITE);
+    //打印当前event_base的网络模型
+    static void printf_base_method(event_base *base){
+        printf("[+]current method is:%s\n",event_base_get_method(base));
+    }
+    //设置event_config的网络模型
+    static bool set_config_method(event_config *config,const char *method){
+        if (event_config_avoid_method(config,method) == 0){
+            return true;
+        }
+        return false;
+    }
+    //设置event_config的flag
+    static void set_config_flags(event_config *config,event_base_config_flag flag){
+        event_config_set_flag(config,flag);
+    }
+    //设置event_config为IOCP
+    static void set_config_IOCP(event_config *config){
+        event_config_set_flag(config,EVENT_BASE_FLAG_STARTUP_IOCP);
+        //初始化IOCP线程数量
+        evthread_use_windows_threads();
+        //设置cpu数量
+        SYSTEM_INFO info;
+        GetSystemInfo(&info);
+        event_config_set_num_cpus_hint(config,(int)(info.dwNumberOfProcessors * 2));
+    }
+
+};
+void read_cb(evutil_socket_t socket, short i, void *userArgs){
+    if (i & EV_TIMEOUT){
+        printf("time out\n");
+        event_free((event *)userArgs);
+        evutil_closesocket(socket);
+        return;
+    }
+    char buf [4096];
+    int len = recv(socket,buf, sizeof(buf),0);
+//    if (len > 0) {
+//        buf[len] = '\0';
+//        printf("[+]%s\n", buf);
+//    }
+    char response[] = "HTTP/1.1 200 OK\r\n"
+                      "Content-Length: 56\r\n"
+                      "Content-Type: text/html\r\n\r\n"
+                      "<html><head></head><body><p>Hello LibEvent</p></body></html>";
+    send(socket,response, sizeof(response),0);
+    event_free((event *)userArgs);
+    evutil_closesocket(socket);
+}
+void client_cb(evutil_socket_t socket, short i, void *userArgs){
+    sockaddr_in cliAddr{};
+    int lenOfSin = sizeof(cliAddr);
+    auto client = (evutil_socket_t)accept(socket, (sockaddr *)&cliAddr, &lenOfSin);
+    //printf("[+]client_cb ip:%s,port:%d\n",inet_ntoa(cliAddr.sin_addr), ntohs(cliAddr.sin_port));
+
+    //客户端读取&&发送
+    event * mEvent_Read = event_new((event_base *) userArgs, client, EV_READ | EV_PERSIST, read_cb, event_self_cbarg());
+    //设定客户端超时
+    timeval t = {3,0};//10s超时
+    event_add(mEvent_Read,&t);
 }
 int main() {
     cSocket socket_;
     socket_.InitWSA();
-    evutil_socket_t listener = socket_.CreateListenSocket();
-    event_base *event_base;
-    event *listen_event;
 
-    event_base = event_base_new();
-    if (event_base == nullptr){
-        fn_ErrorOutput("event_base_new",0);
+    //创建配置上下文
+    event_config *mEventConfig = event_config_new();
+    cEvent::set_config_IOCP(mEventConfig);
+    //初始化配置libevent上下文
+    event_base *mEventBase = event_base_new_with_config(mEventConfig);
+    //释放配置上下文
+    event_config_free(mEventConfig);
+    if (mEventBase == nullptr){
+        fn_ErrorOutput("event_base_new_with_config",0);
+        return -1;
     }
-    //发送大数据再加上EV_WRITE
-    listen_event = event_new(event_base, listener, EV_READ | EV_PERSIST, fn_accept, (void *) event_base);
+    //创建socket
+    evutil_socket_t sock = socket_.CreateListenSocket();
+    event *ev = event_new(mEventBase, sock, EV_READ | EV_PERSIST, client_cb, mEventBase);
+    event_add(ev,nullptr);
 
-    event_add(listen_event, nullptr);
-    event_base_dispatch(event_base);
+    event_base_dispatch(mEventBase);
+    event_free(ev);
+    event_base_free(mEventBase);
     return 0;
 }
 
